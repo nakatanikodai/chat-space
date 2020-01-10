fa90b4b573a4d5239f4a58982da8d7410abed237
nakatanikodai <nakatanixx1123@gmail.com>
2020-01-10 20:57:38 +0900

    コメントアウトの削除
    #不要なコメントアウトを削除しました

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -33,17 +33,3 @@ end
 
 
 
-#def create
-#  respond_to do |format|
-#    format.html { redirect_to group_messages_path, notice: "メッセージを送信しました"}
-#    format.json
-#  @message = @group.messages.new(message_params)
-#  if @message.save
-#    redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
-#  else
-#    @messages = @group.messages.includes(:user)
-#    flash.now[:alert] = 'メッセージを入力して下さい。'
-#    render :index
-#  end
-#  end
-#end
