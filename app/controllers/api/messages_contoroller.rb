5d876f4c7877389b8e63dc59b92833de96692a91
nakatanikodai <nakatanixx1123@gmail.com>
2020-01-19 18:21:32 +0900

    自動更新機能の実装

---
+++ app/controllers/api/messages_contoroller.rb
@@ -0,0 +1,7 @@
+clss Api::MessagesController < ApplicationController
+  def index
+    group = Group.find(params[:group_id])
+    last_message_id = params[:id].to_i
+    @messages = group.messages.includes(:user).where("id > #{last_message_id}")
+  end
+end
