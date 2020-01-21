ff03f40cc01931ef77caae67a2368421491c9bd4
nakatanikodai <nakatanixx1123@gmail.com>
2020-01-21 21:53:14 +0900

    S3への登録

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -60,4 +60,4 @@
 #     # password: "please use keys"
 #   }
 
-server '52.68.32.104', user: 'ec2-user', roles: roles: %w{app db web}
+server '52.68.32.104', user: 'ec2-user', roles:  %w{app db web}
